f1e6bea8167568f95ed243334f236571d20e0e50
joaogui1 <joaoguilhermearujo@gmail.com>
2022-07-30 20:02:09 -0300

    big refactor

--- main.py
+++ main.py
@@ -26,19 +26,20 @@ def main() -> None:
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
     player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon(max_rooms,
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon(
+                                max_rooms,
                                 room_min_size,
                                 room_max_size,
                                 map_width,
                                 map_height, 
                                 max_monsters_per_room,
-                                player)
-
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+                                engine
+                            )
+    engine.update_fov()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -51,8 +52,7 @@ def main() -> None:
         while True:
             engine.render(console=root_console, context=context)
 
-            events = tcod.event.wait()
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 if __name__ == "__main__":
     main()
