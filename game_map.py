ae7d27a2e7edd7ffddfd3951abaf794b3b5f01e8
joaogui1 <joaoguilhermearujo@gmail.com>
2022-08-01 12:57:16 -0300

    another refactor

--- game_map.py
+++ game_map.py
@@ -33,6 +33,10 @@ class GameMap:
 
 
     @property
+    def gamemap(self) -> GameMap:
+        return self
+        
+    @property
     def actors(self) -> Iterator[Actor]:
         """Iterate over this maps living actors."""
         yield from (
